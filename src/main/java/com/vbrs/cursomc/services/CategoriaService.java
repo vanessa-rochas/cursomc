a643e80f60169e2f61f3224bf7b7e29d2cf0a3ae
Vanessa Rocha <vsolgate@gmail.com>
2022-03-03 09:22:17 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/vbrs/cursomc/services/CategoriaService.java
+++ src/main/java/com/vbrs/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.vbrs.cursomc.domain.Categoria;
+import com.vbrs.cursomc.dto.CategoriaDTO;
 import com.vbrs.cursomc.repositories.CategoriaRepository;
 import com.vbrs.cursomc.services.exception.DataIntegrityException;
 import com.vbrs.cursomc.services.exception.ObjectNotFoundException;
@@ -55,5 +56,11 @@ public class CategoriaService {
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), 	orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		
+		return new Categoria(objDto.getId(), objDto.getNome());
+		
 	}
 }
